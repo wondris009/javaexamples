f81b34248eb61756e9c47d7bdb9c14075c59f2f4
vondracek <vondracek@cngroup.dk>
2019-10-22 12:17:23 +0200

    no doc needed, even not the generated one

--- src/main/java/cz/speedygonzales/random/RandomUtils.java
+++ src/main/java/cz/speedygonzales/random/RandomUtils.java
@@ -1,28 +1,12 @@
 package cz.speedygonzales.random;
 
-import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
-/**
- * Created with IntelliJ IDEA.
- * User: vondracek
- * Date: 10.9.13
- * Time: 9:24
- * To change this template use File | Settings | File Templates.
- */
 public class RandomUtils {
 
     private RandomUtils() {
     }
 
-    /**
-     * If fromZero is set then it selects numbers from range 0 - highestNumber
-     * <p>
-     * otherwise
-     * <p>
-     * 1 - highestNumber+1
-     * @return
-     */
     public static int generateRandomNumber(int min, int max) {
         return ThreadLocalRandom.current().nextInt(min, max);
     }
